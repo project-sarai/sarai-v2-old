63e09209a2e7349e703a0e994dd8285d780b34cf
jppcalvan <jpcalvan@up.edu.ph>
2017-06-20 17:14:07 +0800

    Added reports

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -14,6 +14,7 @@ import { Advisories } from '../../api/advisories/advisories.js';
 import { Main } from '../../api/main/main.js';
 import { DSSSettings } from '../../api/weather/sarai-dss-settings.js';
 import { WeatherStations } from '../../api/weather/sarai-weather-stations.js';
+
 Meteor.startup(() => {
   // if the Links collection is empty
   if (Links.find().count() === 0) {
