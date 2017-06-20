f13cc5a3b869c5206a0cb5bc8a5f757cc3530579
jppcalvan <jpcalvan@up.edu.ph>
2017-06-20 17:14:07 +0800

    Added reports

--- imports/ui/components/home/reports.js
+++ imports/ui/components/home/reports.js
@@ -1,7 +1,6 @@
 import { Reports } from '/imports/api/reports/reports.js'
 import { Meteor } from 'meteor/meteor'
 import './reports.html'
-
 Template.Reports.onCreated(function() {
   Meteor.subscribe('reports');
 });
@@ -10,13 +9,6 @@ Template.Reports.helpers({
   reports: function(limit) {
   	var dl = limit || 2;
     return Reports.find({}, {limit: dl});
-  }
-});
-
-Template.Reports.events({
-  'click .more-reports': function(e){
-    e.preventDefault();
-    FlowRouter.go('/reports');
   }
 });
 
