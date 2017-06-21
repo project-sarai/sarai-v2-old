95a239e16194ca6ced3ea52875c8512ada977801
jppcalvan <jpcalvan@up.edu.ph>
2017-06-21 16:59:08 +0800

    Added reports page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -57,13 +57,7 @@ import '../../ui/pages/services-page/services-page.js';
 import '../../ui/pages/reports/reports-page.js';
 import '../../ui/components/home/reports.js';
 
-<<<<<<< 19e36fb2e2e22c0f34c8206c97f64612cd8a5d78
 import '../../ui/pages/advisories/advisories.js';
-
-=======
->>>>>>> Added reports page
-import '../../ui/pages/reports/reports-page.js';
-import '../../ui/components/home/reports.js';
 
 // Set up all routes in the app
 // HOMEPAGE
