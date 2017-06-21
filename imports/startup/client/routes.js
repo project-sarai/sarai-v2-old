5d719905afa16710143093fc15f9b106415e92c4
jppcalvan <jpcalvan@up.edu.ph>
2017-06-21 16:59:08 +0800

    Added reports page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -64,7 +64,8 @@ import '../../ui/components/home/reports.js';
 
 import '../../ui/pages/advisories/advisories.js';
 
-
+import '../../ui/pages/reports/reports-page.js';
+import '../../ui/components/home/reports.js';
 
 
 
