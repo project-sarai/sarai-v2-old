6553d37afdb8a6ec635940325a2cf0f7cca7a5df
jppcalvan <jpcalvan@up.edu.ph>
2017-06-21 13:46:02 +0800

    Fixed reports part

--- imports/ui/components/home/reports.js
+++ imports/ui/components/home/reports.js
@@ -1,6 +1,7 @@
 import { Reports } from '/imports/api/reports/reports.js'
 import { Meteor } from 'meteor/meteor'
 import './reports.html'
+
 Template.Reports.onCreated(function() {
   Meteor.subscribe('reports');
 });
