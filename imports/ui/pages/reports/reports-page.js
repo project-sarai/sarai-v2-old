e770d31f30b195ffdae8133253df898bbd7ace41
jppcalvan <jpcalvan@up.edu.ph>
2017-06-22 11:13:06 +0800

    Fixed reports page

--- imports/ui/pages/reports/reports-page.js
+++ imports/ui/pages/reports/reports-page.js
@@ -7,23 +7,14 @@ Template.ReportsPage.onCreated(function() {
 });
 
 Template.ReportsPage.helpers({
-<<<<<<< 5ec7109a3710ee5f33582d41d77d34ec980b1ae8
 	reports: function() {
     	var data = [];
-=======
-  reports: function() {
-    var data = [];
->>>>>>> Added reports page
-    result = Reports.find({},{sort: {'date': 1}}).forEach(function(item){
+      result = Reports.find({},{sort: {'date': 1}}).forEach(function(item){
       data.push(item);
     })
     return data;
   },
-<<<<<<< 5ec7109a3710ee5f33582d41d77d34ec980b1ae8
  	dateFormat: function(date) {
  		return moment(date).format('MM-DD-YYYY');
  	},
 });
-=======
-});
->>>>>>> Added reports page
